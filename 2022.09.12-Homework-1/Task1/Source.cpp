8e26c654ba07ffb5f6589f7c387b4bc8c4811167
Семён Брувер-Маклаков <bruvermaklakov54@gmail.com>
2022-10-01 18:34:39 +0300

    H/W

--- 2022.09.12-Homework-1/Task1/Source.cpp
+++ 2022.09.12-Homework-1/Task1/Source.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-	int n;
+	int n = 0;
 	std::cin >> n;
 	std::cout << "The next number for the number " << n << " is " << n + 1 << "." << std::endl;
 	std::cout << "The previuos number for the number " << n << " is " << n - 1 << "." << std::endl;
