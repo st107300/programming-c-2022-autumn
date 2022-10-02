b719fd96bd46ff3c8707ea46371a3b3cf50a57cf
Семён Брувер-Маклаков <bruvermaklakov54@gmail.com>
2022-10-02 06:04:34 +0300

    H/W

--- 2022.09.12-Homework-1/Task4/Source.cpp
+++ 2022.09.12-Homework-1/Task4/Source.cpp
@@ -4,6 +4,6 @@ int main(int argc, char* argv[]) {
 	int v = 0;
 	int t = 0;
 	std::cin >> v >> t;
-	std::cout <<(109 + (v * t) % 109) % 109 << std::endl;
+	std::cout << (109 + (v * t) % 109) % 109 << std::endl;
 	return EXIT_SUCCESS;
 }
